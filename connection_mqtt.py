f69b27c5bd51f309923397ffabfbd737ca428815
Estimp <javierchiquitin02@gmail.com>
2025-07-11 13:19:41 +0200

    Reconnection, all-user topic, and .exe with logs
    - Added reconnection in case of failure.
    - Changed topic to accept every user.
    - Added .exe with console logs.

--- connection_mqtt.py
+++ connection_mqtt.py
@@ -13,9 +13,11 @@ def on_message(client, userdata, message):
     show_notification(title, notification_title)
 
 client_id = f"breakify-subscriber-{uuid.uuid4()}"
-client = mqtt.Client(client_id=client_id, clean_session=True, protocol=mqtt.MQTTv311, callback_api_version=2)
-client.connect("localhost", 1883)
-client.subscribe("breakify/notifications/Lucia Caminos/#")
+client = mqtt.Client(client_id=client_id, clean_session=False, protocol=mqtt.MQTTv311, callback_api_version=2)
+client.enable_logger()
+client.reconnect_delay_set(min_delay=1, max_delay=300)
+client.connect("localhost", 1883, keepalive=60)
+client.subscribe("breakify/notifications/#")
 client.on_message = on_message
 
 print("Esperando mensajes...")
